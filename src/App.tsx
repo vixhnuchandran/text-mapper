77b28f1b8c530b541526981f9b2e3a488ff02955
vixhnuchandran <vixhnuchandran@gmail.com>
2024-03-11 07:44:25 +0530

    refactor: Improve file handling, OCR processing, and UI in App.tsx - update file handling to clear previous OCR results - refactor OCR processing to use server-side processing via HTTP request - simplify UI layout and styling - add feature to copy extracted text to clipboard - enhance error handling and logging for debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,48 @@
 import { useState, ChangeEvent } from "react";
-import Tesseract from "tesseract.js";
-import { loadImage, createCanvas } from "canvas";
 import "./App.css";
 
 const App = () => {
   const [imageFile, setImageFile] = useState<File | null>(null);
-  const [ocrResult, setOcrResult] = useState<File | null>(null);
+  const [ocrResult, setOcrResult] = useState<string>("");
   const [extractedText, setExtractedText] = useState<string>("");
   const [loading, setLoading] = useState(false);
+  const [copied, setCopied] = useState(false);
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       setImageFile(e.target.files[0]);
-      setOcrResult(null);
+      setOcrResult(""); // Clear previous OCR result
     }
   };
 
-  const processImage = (imagePath: string) => {
+  const processImageOnServer = async () => {
     setLoading(true);
 
-    loadImage(imagePath).then((image) => {
-      const canvas = createCanvas(image.width, image.height);
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(image, 0, 0);
+    try {
+      const formData = new FormData();
+      formData.append("image", imageFile as Blob);
 
-      Tesseract.recognize(imagePath, "eng")
-        .then(({ data: { words } }) => {
-          ctx.strokeStyle = "red";
-          ctx.lineWidth = 2;
+      const response = await fetch("http://127.0.0.1:8686/ocr-service", {
+        method: "POST",
+        body: formData,
+      });
 
-          for (const word of words) {
-            const { x0, y0, x1, y1 } = word.bbox;
-            ctx.strokeRect(x0, y0, x1 - x0, y1 - y0);
-          }
+      if (response.ok) {
+        const result = await response.json();
+        setExtractedText(result.extractedText);
+        if (result.processedImage) {
+          setOcrResult(result.processedImage);
+        } else {
+          console.error("No processed image received from the server");
+        }
+      } else {
+        console.error("Failed to process image");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
 
-          const extractedText = words.map((word) => word.text).join(" ");
-          setExtractedText(extractedText);
-
-          const dataUrl = canvas.toDataURL("image/png");
-
-          const byteString = atob(dataUrl.split(",")[1]);
-          const mimeString = dataUrl.split(",")[0].split(":")[1].split(";")[0];
-          const ab = new ArrayBuffer(byteString.length);
-          const ia = new Uint8Array(ab);
-          for (let i = 0; i < byteString.length; i++) {
-            ia[i] = byteString.charCodeAt(i);
-          }
-
-          const blob = new Blob([ab], { type: mimeString });
-
-          const file = new File([blob], "processed_image.png");
-
-          setOcrResult(file);
-          setLoading(false);
-        })
-        .catch((error) => console.error("Error:", error))
-        .finally(() => setLoading(false));
-    });
+    setLoading(false);
   };
 
   const handleUpload = () => {
@@ -64,25 +50,25 @@ const App = () => {
       if (ocrResult) {
         downloadFile(ocrResult);
       } else {
-        const imagePath = URL.createObjectURL(imageFile);
-        processImage(imagePath);
+        processImageOnServer();
       }
     }
   };
 
-  const downloadFile = (file: File) => {
-    const originalName = file.name.split(".")[0];
-    const extension = file.name.split(".").pop();
-    const newFileName = `${originalName}out.${extension}`;
+  const copyTextToClipboard = () => {
+    navigator.clipboard.writeText(extractedText);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
 
-    const url = URL.createObjectURL(file);
+  const downloadFile = (file: string) => {
     const link = document.createElement("a");
-    link.href = url;
-    link.setAttribute("download", newFileName);
+    link.href = `data:image/png;base64,${file}`;
+    link.download = "processed_image.png";
     document.body.appendChild(link);
     link.click();
-
-    URL.revokeObjectURL(url);
     document.body.removeChild(link);
   };
 
@@ -93,7 +79,7 @@ const App = () => {
           <div className="mx-auto max-w-7xl lg:px-8">
             <div className="lg:grid lg:grid-cols-2 lg:gap-8">
               <div className="mx-auto max-w-md px-6 sm:max-w-2xl sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
-                <div className="lg:py-24">
+                <div className="lg:py-12">
                   <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl lg:mt-6 xl:text-6xl">
                     <span className="block">Experience the power of</span>
                     <span className="block mt-4 text-indigo-400">
@@ -131,7 +117,22 @@ const App = () => {
                       </div>
                       {/* Add the text area here */}
                       {ocrResult && (
-                        <div className="mt-3 sm:ml-3 sm:mt-0">
+                        <div className="flex flex-col mt-3 sm:ml-3 sm:mt-0 justify-end">
+                          <div className="flex justify-end items-center mt-3">
+                            <button
+                              type="button"
+                              className="bg-gray-300 px-2 py-1 rounded-md text-gray-700 mr-2"
+                              onClick={copyTextToClipboard}
+                            >
+                              Copy
+                            </button>
+                            {copied && (
+                              <span className="bg-black bg-opacity-50 text-white px-2 py-1 rounded-md">
+                                Copied!
+                              </span>
+                            )}
+                          </div>
+
                           <textarea
                             className="w-full h-80 mt-3 px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
                             value={extractedText}
@@ -150,9 +151,9 @@ const App = () => {
                   {ocrResult && (
                     <div>
                       <img
-                        className=" flex justify-center items-center max-w-4xl "
-                        src={URL.createObjectURL(ocrResult)}
-                        alt=""
+                        className="flex justify-center items-center max-w-4xl"
+                        src={`data:image/png;base64,${ocrResult}`}
+                        alt="Processed Image"
                       />
                     </div>
                   )}
